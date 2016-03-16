3f6bfb6b2f58c2add7638c8a96e3916628c1d536
mgarbacz <mich.garbacz@gmail.com>
2016-03-16 02:46:09 -0500

    Refactor constants out into a module

---
+++ app/constants.js
@@ -0,0 +1,28 @@
+// Time Constants
+const MS_IN_SECOND = 1000;
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
+const DAYS_IN_YEAR = 365.25;
+
+// Grid Value Constants
+const WEEKS_IN_YEAR = 52;
+const TOTAL_YEARS = 90;
+const TOTAL_YEARS_IN_MS = TOTAL_YEARS
+  * DAYS_IN_YEAR
+  * HOURS_IN_DAY
+  * MINUTES_IN_HOUR
+  * SECONDS_IN_MINUTE
+  * MS_IN_SECOND;
+
+// Export
+module.exports = {
+  MS_IN_SECOND,
+  SECONDS_IN_MINUTE,
+  MINUTES_IN_HOUR,
+  HOURS_IN_DAY,
+  DAYS_IN_YEAR,
+  WEEKS_IN_YEAR,
+  TOTAL_YEARS,
+  TOTAL_YEARS_IN_MS
+};
