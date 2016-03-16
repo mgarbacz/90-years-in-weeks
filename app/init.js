6bc934131c63ffeb89dd1417ce4ecafee7a2d0b2
mgarbacz <mich.garbacz@gmail.com>
2016-03-16 02:46:36 -0500

    Consume constants from constants module

--- app/init.js
+++ app/init.js
@@ -1,12 +1,4 @@
-const MS_IN_SECOND = 1000;
-const SECONDS_IN_MINUTE = 60;
-const MINUTES_IN_HOUR = 60;
-const HOURS_IN_DAY = 24;
-const DAYS_IN_YEAR = 365.25;
-const WEEKS_IN_YEAR = 52;
-const TOTAL_YEARS = 90;
-
-
+let constants = require('constants');
 let gridElement = document.getElementById('grid');
 
 function weekTemplate() {
@@ -15,7 +7,7 @@ function weekTemplate() {
 
 function yearTemplate() {
   let yearHtml = '<div class="year">';
-  for(let i = 0; i < WEEKS_IN_YEAR; i++) {
+  for(let i = 0; i < constants.WEEKS_IN_YEAR; i++) {
     yearHtml += weekTemplate();
   }
   return yearHtml + '</div>';
@@ -23,7 +15,7 @@ function yearTemplate() {
 
 function gridTemplate() {
   let gridHtml = '';
-  for(let i = 0; i < TOTAL_YEARS; i++) {
+  for(let i = 0; i < constants.TOTAL_YEARS; i++) {
     gridHtml += yearTemplate();
   }
   return gridHtml;
@@ -35,23 +27,18 @@ let birthdateElement = document.getElementById('birthdate');
 let today = new Date();
 today.setHours(0, 0, 0, 0);
 
-const TOTAL_YEARS_MINUS_ONE_DAY_IN_MS = ((TOTAL_YEARS * DAYS_IN_YEAR) - 1)
-  * HOURS_IN_DAY
-  * MINUTES_IN_HOUR
-  * SECONDS_IN_MINUTE
-  * MS_IN_SECOND;
-let fullGridDate = new Date(today.getTime() - TOTAL_YEARS_MINUS_ONE_DAY_IN_MS);
+let fullGridDate = new Date(today.getTime() - constants.TOTAL_YEARS_IN_MS);
 birthdateElement.value = fullGridDate.toISOString().split('T')[0];
 
 function setFilled(event) {
   let birthdate = new Date(birthdateElement.value);
   let milliseconds = today.getTime() - birthdate.getTime();
-  let seconds = milliseconds / MS_IN_SECOND;
-  let minutes = seconds / SECONDS_IN_MINUTE;
-  let hours = minutes / MINUTES_IN_HOUR;
-  let days = hours / HOURS_IN_DAY;
-  let years = days / DAYS_IN_YEAR;
-  let weeks = Math.round(years * WEEKS_IN_YEAR);
+  let seconds = milliseconds / constants.MS_IN_SECOND;
+  let minutes = seconds / constants.SECONDS_IN_MINUTE;
+  let hours = minutes / constants.MINUTES_IN_HOUR;
+  let days = hours / constants.HOURS_IN_DAY;
+  let years = days / constants.DAYS_IN_YEAR;
+  let weeks = Math.round(years * constants.WEEKS_IN_YEAR);
 
 
   var weekBoxElements = document.getElementsByClassName('week');
